5f9b51d07d756be671b0800349b75bb6fa905dc5
Marod <mathews-rodrigues@hotmail.com>
2023-08-18 21:51:52 -0300

    Challenge: starting the challenge

---
+++ src/utils/extract-query-params.js
@@ -0,0 +1,15 @@
+// ?search=Mark&page=2
+// search=Mark&page=2
+// [ 'search=Mark', 'page=2' ]
+	// [ 'search', 'Mark' ]
+	// [ 'page', ' 2' ]
+
+export function extractQueryParams(query) {
+  return query.substr(1).split('&').reduce((queryParams, param) => {
+    const [key, value] = param.split('=')
+  
+    queryParams[key] = value
+
+    return queryParams
+  }, {}) 
+}
